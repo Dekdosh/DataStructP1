4706a6b333d5537f4489b1fe63aa3a9dc4884d2d
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2020-12-14 18:42:01 +0200

    add FunctionsForMain- runtime

---
+++ functionsForMain.h
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <numeric>
+#include <chrono>
+#include <cstdio>
+#include <ratio>
+#include <thread>
+
+void runTimeFunc1();
+void runTimeFunc2();
