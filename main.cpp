f82030770fee43112fa71ca0406cf359cbd2de5c
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2020-12-14 18:42:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-﻿#include "Q1.h"
+#include "Q1.h"
+#include "functionsForMain.h"
 
-int main()//for Q1
+int main()
 {
+    //Q1//
     int n;
     cout << "pls enter n" << endl;
     cin >> n;
@@ -16,19 +18,18 @@ int main()//for Q1
         cout << "pls enter y" << endl;
         cin.getline(y, size);
         if (inputApproved(x, n) && inputApproved(y, n))// only digits
+        {
             cout << multiply(string(x), string(y), n, n) << endl;
+            runTimeFunc1();
+        }
         else
             cout << "Ïnput is not approved" << endl;
     }
     else if (n == 0)
-    {
         cout << "0" << endl;
-    }
     else
-    {
-        cout << "Not a valid n" << endl;// since we input n as int, if it will start with zeros, 
-                                        //it wont count and therefore the input from that prespective will be OK
-    }
-
+        cout << "Not a valid n" << endl;
+    //Q1//
     return 0;
 }
+
