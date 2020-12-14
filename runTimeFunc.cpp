7def7b619b9c279f6a66e594b814eb0ddd7cba44
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2020-12-14 18:46:29 +0200

    add runTimeFunc

---
+++ runTimeFunc.cpp
@@ -0,0 +1,34 @@
+#include "Q1.cpp"
+#include "functionsForMain.h"
+
+void runTimeFunc1()
+{
+	auto start = chrono::high_resolution_clock::now();
+	// unsync the I/O of C and C++.
+	int i = ios_base::sync_with_stdio(false);// CHECK! I added the in i= , unless id does not work!
+	string multiply();// Here you put the name of the function you wish to measure
+	//auto end = chrono::high_resolution_clock::now(); --> I put below since it gave me an error the way they did it :(
+	// Calculating total time taken by the program.
+	double time_taken = chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - start).count();
+	double new_time_taken = (time_taken * 1e-9);
+	ofstream myfile("Measure.txt"); // The name of the file
+	myfile << "Time taken by function multiply is : " << fixed << new_time_taken << setprecision(9);
+	myfile << " sec" << endl;
+	myfile.close();
+}
+
+void runTimeFunc2()
+{
+	auto start = chrono::high_resolution_clock::now();
+	// unsync the I/O of C and C++.
+	int i = ios_base::sync_with_stdio(false);// CHECK! I added the in i= , unless id does not work!
+	string karatsubaRec();// Here you put the name of the function you wish to measure
+	//auto end = chrono::high_resolution_clock::now(); --> I put below since it gave me an error the way they did it :(
+	// Calculating total time taken by the program.
+	double time_taken = chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - start).count();
+	double new_time_taken = (time_taken * 1e-9);
+	ofstream myfile("Measure.txt"); // The name of the file
+	myfile << "Time taken by function karatsubaRec is : " << fixed << new_time_taken << setprecision(9);
+	myfile << " sec" << endl;
+	myfile.close();
+}
